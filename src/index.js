d55671ea87f02bea51802dc9f0ab6c52d0f91306
Kamil <isaevkamil029@mail.ru>
2022-12-10 13:12:19 +0300

    Bug Fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM
     .createRoot(document.getElementById('root'))
     .render(
       <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={window.location.pathname || ''}>
           <App />
         </BrowserRouter>
     </React.StrictMode>
