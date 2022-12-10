f83c4e28595a67ee8be7c26bdcfc49f6a14d4c40
Kamil <isaevkamil029@mail.ru>
2022-12-10 13:46:21 +0300

    bug Fix

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,7 +19,7 @@ const App = () => {
                 <main>
                     <Suspense fallback={<Spinner/>}>
                         <Switch>
-                            <Route exact path="/" >
+                            <Route exact path="/" component={MainPage}>
                                 <MainPage/>
                             </Route>
                             <Route exact path="/comics">
